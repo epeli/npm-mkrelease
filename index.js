f0d9ecb4a728a410456d7d43778996a8e2a1b09c
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-04-11 22:16:43 +0300

    Use npm to detect current release version. And make sure user enters proper semver

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ var fs = require("fs");
 var spawn = require('child_process').spawn;
 var readline = require('readline');
 
+var request = require("request");
+var semver = require("semver");
+
+function fetchCurrentNpmRelease(pkg, cb) {
+  request("http://registry.npmjs.org/" + pkg, function(err, res, body) {
+    if (err) return cb(err);
+    var data = JSON.parse(body);
+    var versions = Object.keys(data.versions).sort(semver.compare);
+    cb(null, versions[versions.length-1]);
+  });
+}
+
+function askSemver(cb) {
+  rl.question("New version> ", function(version) {
+    if (semver.valid(version)) return cb(null, version);
+    askSemver(cb);
+  });
+}
 
 
 function execute(){
@@ -37,23 +55,27 @@ var rl = readline.createInterface({
 });
 
 var packageJSON = JSON.parse(fs.readFileSync("./package.json").toString());
-console.log("Current version is", packageJSON.version);
 
-rl.question("New version> ", function(version) {
-  packageJSON.version = version.trim() || packageJSON.version;
-  fs.writeFileSync("./package.json", JSON.stringify(packageJSON, null, "  "));
+console.log("Version in package.json:", packageJSON.version);
+fetchCurrentNpmRelease(packageJSON.name, function(err, npmVersion) {
+  console.log("Current npm release:", npmVersion);
 
-  execute(
-    [ "npm", "publish" ],
-    [ "git", "commit", "package.json", "-m", "Release " + version ],
-    [ "git", "tag", "-a", version, "-m", "Release " + version ],
-    [ "git", "push", "--tags", "origin"],
-    [ "git", "push", "origin", "master"],
-    function(err){
-      if (err) throw err;
-      rl.close();
-      console.log("DONE");
-    }
-  );
+  askSemver(function(version) {
+    packageJSON.version = version;
+    fs.writeFileSync("./package.json", JSON.stringify(packageJSON, null, "  "));
 
+    execute(
+      [ "npm", "publish" ],
+      [ "git", "commit", "package.json", "-m", "Release " + version ],
+      [ "git", "tag", "-a", version, "-m", "Release " + version ],
+      [ "git", "push", "--tags", "origin"],
+      [ "git", "push", "origin", "master"],
+      function(err){
+        if (err) throw err;
+        rl.close();
+        console.log("DONE");
+      }
+    );
+
+  });
 });
